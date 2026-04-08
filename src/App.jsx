c649c8ec5e41c362cf8e445e6aec4718eee8a271
allyparrk <allypark2005@icloud.com>
2026-04-08 10:15:36 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,12 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
 import SiteNav from './components/SiteNav'
 import Footer from './components/Footer'
-import HomePage from './pages/HomePage'
-import SearchPage from './pages/SearchPage'
-import AboutPage from './pages/AboutPage'
 import CafeDetailPage from './pages/CafeDetailPage'
 
 function App() {
   return (
     <>
       <SiteNav />
-      <main>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/search" element={<SearchPage />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/cafe/:id" element={<CafeDetailPage />} />
-        </Routes>
-      </main>
+      <CafeDetailPage />
       <Footer />
     </>
   )
